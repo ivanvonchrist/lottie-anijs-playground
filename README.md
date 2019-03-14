4f7de9fa11df4af75213d2dbde31f92b098d5dff
ivan <ivanvonchrist@gmail.com>
2019-03-14 14:58:03 +1000

    Initial set-up and testing

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # lottie-anijs-playground
 Playground for checking out Lottie and AniJS!
+
+[See the playground live on github.io](https://ivanvonchrist.github.io/lottie-anijs-playground/)
---
+++ js/index.js
@@ -0,0 +1,7 @@
+lottie.loadAnimation({
+    container: document.getElementById('example'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: '../data/example.json'
+})
